15c0181a20366ceba2b5aa0d98191635077cce38
Chase Meyer <chase.e.meyer@gmail.com>
2020-10-04 16:48:34 -0500

    skeleton app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,72 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+const useStyles = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
 
 class App extends Component {
+  // classes = useStyles;
+
   state = {
     values: [],
   };
 
   componentDidMount() {
-    this.setState({
-      values: [
-        { id: 1, name: "Value 101" },
-        { id: 2, name: "Value 102" },
-      ],
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      });
     });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} alt="logo" className="App-logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" >
+              Ecommerce
+          </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <List component="nav">
+          {this.state.values.map((value: any) => (
+            <ListItem key={value.id} button>
+              <ListItemText primary={value.name} />
+            </ListItem>
+          ))}
+        </List>
       </div>
     );
   }
