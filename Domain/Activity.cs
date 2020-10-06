c01fbe81d3b95f86c66529d835c61708fb10cb07
Chase Meyer <chase.e.meyer@gmail.com>
2020-10-06 16:48:36 -0500

    CRUD Skeleton

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,7 +1,7 @@
-namespace Domain
-{
-    public class Activity
-    {
+using System;
+
+namespace Domain {
+    public class Activity {
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
---
+++ Persistence/Seed.cs
@@ -0,0 +1,114 @@
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using System;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {
+            if (!context.Activities.Any())
+            {
+                var activities = new List<Activity>
+                {
+                    new Activity
+                    {
+                        Title = "Past Activity 1",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 2",
+                        Date = DateTime.Now.AddMonths(-1),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 3",
+                        Date = DateTime.Now.AddMonths(-3),
+                        Description = "Activity 3 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 4",
+                        Date = DateTime.Now.AddMonths(-8),
+                        Description = "Activity 8 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 5",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 6",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 7",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 8",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 9",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 10",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "London",
+                        Venue = "Pub",
+                    },
+                };
+
+                context.Activities.AddRange(activities);
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
